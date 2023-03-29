8dffd1e965456e85974cde7d7ab0f4eb77a6b3d6
hrebeniev <nick.hrebeniev@gmail.com>
2023-03-29 22:22:45 +0300

    Library

---
+++ src/main/java/com/hillel/hrebeniev/MathLibrary.java
@@ -0,0 +1,10 @@
+package com.hillel.hrebeniev;
+
+public class MathLibrary {
+    public static void MathPow(double a, double b){
+        System.out.println(Math.pow(a, b));
+    }
+    public static void MathSum(double a, double b){
+        System.out.println(a + b);
+    }
+}
